2e8cf17520c1f6f9f0c8b00320f2226249bc51ed
Patrik Reizinger <prmedia17@gmail.com>
2022-11-01 12:18:17 +0000

    cluster tools submodule -> scripts

--- scripts/get_used_space.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-awk '/[0-9]+,preizinger/{print}' /mnt/qb/work/bethge/bethge_user_disk_usage_MB.log
--- scripts/start_interactive_job.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash 
-PARTITION=gpu-2080ti-interactive 
-FLAGS=--exclude=slurm-bm-06
-#FLAGS= 
-srun --job-name="$JOB_NAME" --partition=$PARTITION --pty --gres=gpu:1 "$FLAGS" -- ./scripts/interactive_job_inner.sh 
-
