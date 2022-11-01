55ed2010e5af229d7fe7e9b6ecc4733ba3b17af0
Patrik Reizinger <prmedia17@gmail.com>
2022-11-01 15:34:59 +0000

    vae with sam and test

--- vae_sam/cli.py
+++ vae_sam/cli.py
@@ -1,17 +1,12 @@
-from pytorch_lightning.utilities.cli import LightningCLI
-from pytorch_lightning.loggers.wandb import WandbLogger
-from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
-
-
-# from vae_sam.runners.runner import SAMModule
-from vae_sam.utils import add_tags
 from pl_bolts.datamodules import CIFAR10DataModule
-from vae_sam.models.ivae import VAE
+from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
+from pytorch_lightning.utilities.cli import LightningCLI
+
+from vae_sam.models.vae import VAE
 
 
-class MyLightningCLI(LightningCLI):
+class SAMLightningCLI(LightningCLI):
     def add_arguments_to_parser(self, parser):
-
         parser.add_argument(
             "--notes",
             type=str,
@@ -29,46 +24,25 @@ class MyLightningCLI(LightningCLI):
         parser.add_lightning_class_args(EarlyStopping, "early_stopping")
         parser.set_defaults(
             {
-                "early_stopping.monitor": "Metrics/val/neg_elbo",
+                "early_stopping.monitor": "val_loss",
                 "early_stopping.mode": "min",
                 "early_stopping.patience": 5,
             }
         )
 
-    def before_instantiate_classes(self) -> None:
-        pass
 
-        # self.config[self.subcommand].trainer.logger.init_args.tags = add_tags(
-        #     self.config[self.subcommand]
-        # )
-
-    def before_fit(self):
-        if isinstance(self.trainer.logger, WandbLogger) is True:
-            # required as the parser cannot parse the "-" symbol
-            self.trainer.logger.__dict__["_wandb_init"]["entity"] = "ima-vae"
-
-            if self.config[self.subcommand].model.offline is True:
-                self.trainer.logger.__dict__["_wandb_init"]["mode"] = "offline"
-            else:
-                self.trainer.logger.__dict__["_wandb_init"]["mode"] = "online"
-
-            # todo: maybe set run in the CLI to false and call watch before?
-            self.trainer.logger.watch(self.model, log="all", log_freq=250)
-
-
-cli = MyLightningCLI(
+cli = SAMLightningCLI(
     VAE,
     CIFAR10DataModule,
     save_config_callback=None,
     run=True,
-    # parser_kwargs={"parse_as_dict": False},
-    # trainer_defaults={
-    #     "callbacks": [
-    #         ModelCheckpoint(
-    #             save_top_k=1,
-    #             monitor="Metrics/val/neg_elbo",
-    #             mode="min",
-    #         ),
-    #     ]
-    # },
+    trainer_defaults={
+        "callbacks": [
+            ModelCheckpoint(
+                save_top_k=1,
+                monitor="val_loss",
+                mode="min",
+            ),
+        ]
+    },
 )
--- vae_sam/utils.py
+++ vae_sam/utils.py
@@ -1,19 +1,5 @@
-def add_tags(args):
-    try:
-        args.tags
-    except:
-        args.tags = []
+from os.path import dirname, abspath, join
 
-    if args.tags is None:
-        args.tags = []
 
-    if args.model.prior == "uniform":
-        args.tags.append("uniform_prior")
-    elif args.model.prior == "gaussian":
-        args.tags.append("gaussian_prior")
-    elif args.model.prior == "beta":
-        args.tags.append("beta_prior")
-    elif args.model.prior == "laplace":
-        args.tags.append("laplace_prior")
-
-    return list(set(args.tags))
+DATA_DIR = join(dirname(dirname(abspath(__file__))), "data")
+CIFAR10_DIR = join(DATA_DIR, "cifar10")
