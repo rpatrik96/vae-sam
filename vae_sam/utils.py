f3ebf86913eef40202bda95857580492a68c4b12
Patrik Reizinger <prmedia17@gmail.com>
2022-11-03 13:23:28 +0000

    wandb init

--- vae_sam/utils.py
+++ vae_sam/utils.py
@@ -3,3 +3,18 @@ from os.path import dirname, abspath, join
 
 DATA_DIR = join(dirname(dirname(abspath(__file__))), "data")
 CIFAR10_DIR = join(DATA_DIR, "cifar10")
+
+
+def add_tags(args):
+    try:
+        args.tags
+    except:
+        args.tags = []
+
+    if args.tags is None:
+        args.tags = []
+
+    if args.model.sam_update is True:
+        args.tags.append("sam")
+
+    return list(set(args.tags))
