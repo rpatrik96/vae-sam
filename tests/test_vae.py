137971a7df6ed082eb9f8331b395a91e1cbb0aff
Patrik Reizinger <prmedia17@gmail.com>
2022-11-14 15:31:35 +0000

    rec loss for sam shoould be higher in tests

--- tests/test_vae.py
+++ tests/test_vae.py
@@ -10,4 +10,6 @@ def test_sam_update():
     x = torch.randn((batch_size, *CIFAR10DataModule.dims))
 
     z, z_mu, x_hat, p, q = vae._run_step(x)
-    recon_loss = vae.rec_loss(z_mu, x, x_hat)
+    recon_loss, recon_loss_sam = vae.rec_loss(z_mu, x, x_hat)
+
+    assert recon_loss < recon_loss_sam
