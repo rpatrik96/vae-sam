72f93ebfb6e97763c8998f1c84e8cef85a8e742e
Patrik Reizinger <prmedia17@gmail.com>
2022-11-21 11:47:50 +0000

    sam linear loss breaking test

--- tests/test_vae.py
+++ tests/test_vae.py
@@ -13,3 +13,23 @@ def test_sam_update():
     recon_loss, recon_loss_sam = vae.rec_loss(z_mu, x, x_hat)
 
     assert recon_loss < recon_loss_sam
+
+
+def test_sam_linear_loss():
+    batch_size = 8
+    vae = VAE(sam_update=True)
+    loss = lambda n, m: (n - m).mean()
+    x = torch.randn((batch_size, *CIFAR10DataModule.dims))
+
+    z, z_mu, x_hat, p, q = vae._run_step(x)
+
+    dLdz, scale = vae.sam_step(x, z_mu, loss=loss)
+
+    x_hat = vae.decoder(z_mu)
+    x_hat_sam = vae.decoder(z_mu + scale * dLdz)
+
+    dLdz = torch.autograd.grad(outputs=loss(x, x_hat), inputs=z_mu)[0].detach()
+
+    dLdz_sam = torch.autograd.grad(outputs=loss(x, x_hat_sam), inputs=z_mu)[0].detach()
+
+    assert dLdz.mean() == dLdz_sam.mean()
