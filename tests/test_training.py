8c1d96ea6ee286edc0a2f75832057d3ce558558e
Patrik Reizinger <prmedia17@gmail.com>
2023-03-15 08:20:47 +0000

    dev_run test with fix enc_var

--- tests/test_training.py
+++ tests/test_training.py
@@ -2,11 +2,13 @@ import torch
 from pytorch_lightning.trainer import Trainer
 
 from vae_sam.models.vae import VAE
+import pytest
 
 
-def test_fast_dev_run(cifar10dm):
+@pytest.mark.parametrize("enc_var", [None, 1.0])
+def test_fast_dev_run(cifar10dm, enc_var):
     AVAIL_GPUS = min(1, torch.cuda.device_count())
-    model = VAE()
+    model = VAE(enc_var=enc_var)
     trainer = Trainer(
         fast_dev_run=True,
         gpus=AVAIL_GPUS,
@@ -14,9 +16,10 @@ def test_fast_dev_run(cifar10dm):
     trainer.fit(model=model, datamodule=cifar10dm)
 
 
-def test_fast_dev_run_sampled(cifar10dm):
+@pytest.mark.parametrize("enc_var", [None, 1.0])
+def test_fast_dev_run_sampled(cifar10dm, enc_var):
     AVAIL_GPUS = min(1, torch.cuda.device_count())
-    model = VAE(val_num_samples=4)
+    model = VAE(val_num_samples=4, enc_var=enc_var)
     trainer = Trainer(
         fast_dev_run=True,
         gpus=AVAIL_GPUS,
